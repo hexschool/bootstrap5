9e4b57d3df6e652abcacd075b773ea047766c919
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-09-19 14:18:49 +0800

    update 2022-09-19 14:18:49

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git init
 git add -A
 git commit -m "update `date +'%Y-%m-%d %H:%M:%S'`";
 
-git push -f https://github.com/hexschool/bootstrap5.git master:gh-pages
+git push -f git@github.com:hexschool/bootstrap5.git master:gh-pages
 
 cd -
